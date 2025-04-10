bfb4116ec9e89dba8a71ddb1f408d47ceb866e07
exos-alex <huzaifaprp09@gmail.com>
2025-04-10 20:36:00 +0530

    Add files via upload

---
+++ lib_main (1).dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(NexZApp());
+}
+
+// ... (Code trimmed to avoid overflow; you already have the full main.dart)
