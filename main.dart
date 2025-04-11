c8b539ebb8d96d7e2eeffddb39ca41a70bfaaf19
exos-alex <huzaifaprp09@gmail.com>
2025-04-11 16:57:03 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:flutter_tts/flutter_tts.dart';
-
-void main() {
-  runApp(NexZApp());
-}
-
-// ... (Code trimmed to avoid overflow; you already have the full main.dart)
